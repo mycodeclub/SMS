9362f8dcefef89869c51decf83242f3cb0354b8d
Mahima Yadav <mahicodepoint1>
2025-04-28 18:08:05 +0530

    changes ParentDetail

--- SchoolManagement/Areas/Admin/Controllers/StandardsController.cs
+++ SchoolManagement/Areas/Admin/Controllers/StandardsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using SchoolManagement.Data;
 using SchoolManagement.Models;
+using SchoolManagement.Models.User;
 using SchoolManagement.Services;
 
 namespace SchoolManagement.Areas.Admin.Controllers
@@ -40,55 +41,44 @@ namespace SchoolManagement.Areas.Admin.Controllers
             return View();
         }
 
-        // POST: Staff/Standards/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create( Standard standard)
+        public async Task<IActionResult> Create(Standard standard)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(standard);
-                await _context.SaveChangesAsync();
+                await _standardService.GetStandards(standard.UniqueId);
+
+                {
+
+                    _context.Update(standard);
+                }
+
                 return RedirectToAction(nameof(Index));
             }
             return View(standard);
         }
-         
-        // GET: Staff/Standards/Delete/5
+
+
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
-            {
                 return NotFound();
-            }
 
-            var standard = await _context.Standards
-                .FirstOrDefaultAsync(m => m.UniqueId == id);
+            var standard = await _standardService.GetStandardById(id.Value);
             if (standard == null)
-            {
                 return NotFound();
-            }
 
             return View(standard);
         }
 
-        // POST: Staff/Standards/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var standard = await _context.Standards.FindAsync(id);
-            if (standard != null)
-            {
-                _context.Standards.Remove(standard);
-            }
-
-            await _context.SaveChangesAsync();
+            await _standardService.GetStandardById(id);
             return RedirectToAction(nameof(Index));
         }
-
         private bool StandardExists(int id)
         {
             return _context.Standards.Any(e => e.UniqueId == id);
